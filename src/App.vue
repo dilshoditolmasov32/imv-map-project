296ea6612d6ca314d047e4ee0a8786d1f97d9601
unknown <dilshoditolmasov32@gmail.com>
2026-01-06 13:38:26 +0500

    fix vite base path

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
-
-<p>
-  Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quam maiores adipisci, eos inventore aliquam quae expedita maxime, asperiores, eius enim dolore ipsam velit sint nam distinctio deserunt ab saepe!
-</p>
-<img src="../src/assets/grand-logo.svg" alt="logo iconka">
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quam
+    maiores adipisci, eos inventore aliquam quae expedita maxime, asperiores,
+    eius enim dolore ipsam velit sint nam distinctio deserunt ab saepe!
+  </p>
+  <img src="../src/assets/grand-logo.svg" alt="logo iconka" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>

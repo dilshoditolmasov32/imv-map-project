ba805c03513aceb0478c4342d81eed25c983e705
unknown <dilshoditolmasov32@gmail.com>
2026-01-06 13:26:19 +0500

    project is updated

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 <p>
   Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quam maiores adipisci, eos inventore aliquam quae expedita maxime, asperiores, eius enim dolore ipsam velit sint nam distinctio deserunt ab saepe!
 </p>
-<img src="../src/assets/grand-logo.svg" alt="">
+<img src="../src/assets/grand-logo.svg" alt="logo iconka">
 </template>
 
 <style scoped>

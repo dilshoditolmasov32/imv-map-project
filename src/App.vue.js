2c10fb488ff8b71d5e45367e7f7d9dd7f6e9868d
unknown <dilshoditolmasov32@gmail.com>
2026-01-06 12:27:27 +0500

    map project is created

---
+++ src/App.vue.js
@@ -0,0 +1,7 @@
+var __VLS_ctx = {};
+var __VLS_components;
+var __VLS_intrinsics;
+var __VLS_directives;
+__VLS_asFunctionalElement1(__VLS_intrinsics.h1, __VLS_intrinsics.h1)({});
+var __VLS_export = (await import('vue')).defineComponent({});
+export default {};

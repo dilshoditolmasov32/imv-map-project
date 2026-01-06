c90877d1077fada549a632c6541ce51ec9f48452
unknown <dilshoditolmasov32@gmail.com>
2026-01-06 14:27:03 +0500

    fix vite app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+
+</script>
 
 <template>
   <h1>hello world</h1>
   <img src="../src/assets/grand-logo.svg" alt="logo iconka" />
 </template>
 
-<style scoped></style>
+<style scoped>
+  
+</style>
